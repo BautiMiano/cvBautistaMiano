48c08acd4e47b7667236d9dd9ca8cc17fc0c4502
BautiMiano <114349383+BautiMiano@users.noreply.github.com>
2023-11-13 11:35:40 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,3 +1,25 @@
-function mostrarCurriculum() {
-    alert("A continuación veran  mi Curriculum");
+const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+
+let intentos = 3;
+while (intentos > 0) {
+
+  const intentoUsuario = prompt(`Adivina el número (entre 1 y 10). Intentos restantes: ${intentos}`);
+
+  const numeroUsuario = parseInt(intentoUsuario);
+
+  if (numeroUsuario === numeroAleatorio) {
+    alert("¡Adivinaste!");
+    break;
+  } else {
+    alert(`Incorrecto. Intentos restantes: ${intentos - 1}`);
+    intentos--;
+
+    if (intentos === 0) {
+      alert(`Lo siento, el número correcto era ${numeroAleatorio}. ¡Mejor suerte la próxima vez!`);
+    }
   }
+}
+
+function mostrarCurriculum() {
+  alert("A continuación veran  mi Curriculum");
+}
