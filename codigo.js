078596e959547503584c584ee7d5f30e6bb71c09
BautiMiano <114349383+BautiMiano@users.noreply.github.com>
2023-11-23 22:53:19 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,25 +1,37 @@
-const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+let numeroAleatorio = generarNumeroAleatorio(); // Generar un número aleatorio entre 1 y 20
+let intentos = 5;
 
-let intentos = 3;
-while (intentos > 0) {
-
-  const intentoUsuario = prompt(`Adivina el número (entre 1 y 10). Intentos restantes: ${intentos}`);
-
-  const numeroUsuario = parseInt(intentoUsuario);
-
-  if (numeroUsuario === numeroAleatorio) {
-    alert("¡Adivinaste!");
-    break;
-  } else {
-    alert(`Incorrecto. Intentos restantes: ${intentos - 1}`);
-    intentos--;
-
-    if (intentos === 0) {
-      alert(`Lo siento, el número correcto era ${numeroAleatorio}. ¡Mejor suerte la próxima vez!`);
-    }
-  }
+function generarNumeroAleatorio() {
+  return Math.floor(Math.random() * 20) + 1;
 }
 
-function mostrarCurriculum() {
-  alert("A continuación veran  mi Curriculum");
+function reiniciarJuego() {
+  numeroAleatorio = generarNumeroAleatorio();
+  intentos = 5;
+  document.getElementById('numeroUsuario').value = '';
+}
+
+function adivinarNumero() {
+  const numeroUsuario = parseInt(document.getElementById('numeroUsuario').value);
+
+  if (isNaN(numeroUsuario) || numeroUsuario < 1 || numeroUsuario > 20) {
+    alert("Por favor, ingresa un número válido entre 1 y 20.");
+    return;
+  }
+
+  intentos--;
+
+  if (numeroUsuario === numeroAleatorio) {
+    alert(`¡Felicidades! Adivinaste el número ${numeroAleatorio} en ${5 - intentos} intentos🎉🎉.`);
+    reiniciarJuego();
+  } else {
+    let pista = (numeroUsuario < numeroAleatorio) ? "El número a adivinar es mayor." : "El número a adivinar es menor.";
+    
+    if (intentos === 0) {
+      alert(`Lo siento, has agotado tus intentos. El número a adivinar era ${numeroAleatorio}. ¡Mejor suerte la próxima vez!`);
+      reiniciarJuego();
+    } else {
+      alert(`${pista} Intentos restantes: ${intentos}`);
+    }
+  }
 }
